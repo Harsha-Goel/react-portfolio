7f05f73046d2709030c2dabd57b1ad8f0b7a0cb8
Harsha Goel <harshagoyal868@gmail.com>
2023-08-17 18:34:19 +0530

    fifth commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -29,7 +29,7 @@ return (
     <>
         <div className="container contact-page">
             <div className="text-zone">
-                <h1>
+                <h1 className="har">
                     <AnimatedLetters 
                         strArray={['C','o','n','t','a','c','t',' ','m','e']}
                         idx={15}
@@ -41,7 +41,7 @@ return (
                 </p>
                 <div className="contact-form">
                     <form ref={refForm} onSubmit={sendEmail}>
-                        <ul>
+                        <ul className="formed">
                             <li className="half">
                                 <input type="text" name="from_name" placeholder="Name" required />
                             </li>
