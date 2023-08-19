da059bc13c84601a05ad469074e943e12122d685
Harsha Goel <harshagoyal868@gmail.com>
2023-08-19 14:55:31 +0530

    fifth commit

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,27 +6,34 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faHashtag, faLink } from '@fortawesome/free-solid-svg-icons'
 const Project = () => {
   return (
-    <div>
+    <div className="pro-page">
+    <div className="project">
       <h1 className="head">What I've built :</h1>
-      <div className="project">
-        <div className="p1">
-            <FontAwesomeIcon className="fo" icon={faHashtag} color="#A69BB6"  />
+      
             <div className="flex">
+            <FontAwesomeIcon className="fo" icon={faHashtag} color="#A69BB6"  />
             <div>
+            <a href="https://react-portfolio-flax-nine.vercel.app/"  target="_blank"> 
               <img className="pro" src={ProjectOne} alt="One" />
+              </a>
             </div>
+            <div className="info1">
             <div className="info">
               <h2>It is my first project. I made a portfolio with all my heart.</h2>
-              <h3>Skills Used: </h3>
-              <span className="spo">HTML</span>
-              <span className="spo">CSS</span>
+              <h3 className="head1">Skills Used: </h3>
+              <div className="skill">
+              <span className="spo spo1">HTML</span>
+              <span className="spo spo1">CSS</span>
               
-              <span className="spo">Javascript</span>
-              <span className="spo">ReactJs</span>
-              <span className="spo">Animations</span>
-              <h3>Links:</h3>
+              <span className="spo spo1">Javascript</span>
+              <span className="spo spo1">ReactJs</span>
+              <span className="spo spo1">Animations</span>
+              </div>
+              <div className="info3">
+              <h3 className='head2' id='head3'>Links:</h3>
               <a
                 className="spo"
+                id="head4"
                     target="_blank"
                     rel="noreferrer"
                     href="https://github.com/Harsha-Goel/react-portfolio"
@@ -35,7 +42,8 @@ const Project = () => {
                    <span> Git Link</span>
                 </a>
                 <a
-                className="spo"
+                className="spo spo2"
+                id="head4"
                     target="_blank"
                     rel="noreferrer"
                     href="https://react-portfolio-flax-nine.vercel.app/"
@@ -43,28 +51,35 @@ const Project = () => {
                 <FontAwesomeIcon icon={faLink} color="pink" />
                    <span> Live Link</span>
                 </a>
+                </div>
           </div>
           </div>
-        </div>
-        
-        <div className="p1">
+          </div>
+       
+        <div className="flex flex2" id="flex1">
         <FontAwesomeIcon className="fo" icon={faHashtag} color="#A69BB6"  />
-        <div className="flex">
           <div>
+          <a href="https://github.com/Harsha-Goel/new-code" target="_blank">
             <img className="pro" src={ProjectTwo} alt="Two" />
+            </a>
           </div>
+          <div className="info1">
           <div className="info">
             <h2>I made a Realtime CodeEditor. Here Many people can work at same time.</h2>
-            <h3>Skills Used: </h3>
-            <span className="spo">HTML</span>
-            <span className="spo">CSS</span>
-            <span className="spo">Javascript</span>
-            <span className="spo">ReactJs</span>
-            <span className="spo">NodeJs</span>
-            <span className="spo">Web Sockets</span>
-            <h3>Links :</h3>
+            <h3 className="head1">Skills Used: </h3>
+            <div className="skill">
+            <span className="spo1 spo">HTML</span>
+            <span className="spo1  spo">CSS</span>
+            <span className="spo1 spo">Javascript</span>
+            <span className="spo1 spo">ReactJs</span>
+            <span className="spo1 spo">NodeJs</span>
+            <span className="spo1 spo">Web Sockets</span>
+            </div>
+            <div className="info3">
+            <h3 className='head2' id='head6'>Links:</h3>
             <a
                 className="spo"
+                id="head4"
                     target="_blank"
                     rel="noreferrer"
                     href="https://github.com/Harsha-Goel/new-code"
@@ -72,10 +87,11 @@ const Project = () => {
                 <FontAwesomeIcon icon={faLink} color="pink" />
                    <span> Git Link</span>
                 </a>
-                
+                </div>
           </div>
           </div>
-        </div>
+          </div>
+      
       </div>
     </div>
   )
